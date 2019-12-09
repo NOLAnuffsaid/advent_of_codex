69cf7285711627bfa25c8607be52d2aee7910372
W Millon <wjmillon@gmail.com>
2019-12-09 04:36:40 -0600

    Start

---
+++ lib/mix/tasks/2015/day_1.ex
@@ -0,0 +1,18 @@
+defmodule Mix.Tasks.TwentyFifteen.Day1 do
+  use Mix.Task
+
+  @shortdoc "Advent Of Code 2015 Day 1"
+
+  @moduledoc """
+  Attempts to solve the Advent Of Code 2015 Day 1 challenge.
+
+  ## Example
+
+  mix 2015.day1 input
+  """
+
+  require IEx
+
+  @impl Mix.Task
+  def run(_args), do: IO.puts "Solving problems"
+end
